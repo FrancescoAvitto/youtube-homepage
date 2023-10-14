e7c6d7d0d8b3be6eeb8ce2f0b1936b807a5f262c
FrancescoAvitto <91961441+FrancescoAvitto@users.noreply.github.com>
2023-10-14 23:56:40 +0200

    initial code, 0h 54min, header, scrollbar x, buttons

---
+++ src/data/home.ts
@@ -0,0 +1,16 @@
+export const categories = [
+    "All",
+    "Javascript",
+    "Typescript",
+    "Programming",
+     "Weight Lifting",
+    "Bowling",
+     "Hiking",
+    "React",
+     "Next.js",
+    "Functional Programming",
+     "Object Oriented Programming",
+    "Frontend Web Development",
+    "Web Development",
+    "Coding"
+]
