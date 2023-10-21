f0cdc96b4c44f5428b7343c832cae575f6ebf041
FrancescoAvitto <91961441+FrancescoAvitto@users.noreply.github.com>
2023-10-21 19:55:21 +0200

    context in sidebar and change img of items

--- src/data/home.ts
+++ src/data/home.ts
@@ -18,129 +18,114 @@ export const categories = [
 export const videos = [
     {
       id: "1",
-      title: "CSS Anchor Is The Best New CSS Feature Since Flexbox",
+      title: "Best Ramen in Tokyo",
       channel: {
-        name: "Web Dev Simplified",
-        id: "WebDevSimplified",
-        profileUrl:
-          "https://yt3.ggpht.com/ytc/APkrFKZWeMCsx4Q9e_Hm6nhOOUQ3fv96QGUXiMr1-pPP=s48-c-k-c0x00ffffff-no-rj",
-      },
+        name: "Travel in Japan",
+        id: "TravelInJapan",
+        profileUrl: "https://picsum.photos/200"      },
       views: 222536,
       postedAt: new Date("2023-08-29"),
       duration: 938,
-      thumbnailUrl: "https://i.ytimg.com/vi/B4Y9Ed4lLAI/maxresdefault.jpg",
+      thumbnailUrl: "https://picsum.photos/380/220",
       videoUrl:
         "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
     },
     {
       id: "2",
-      title: "NEW Way To Create Variables In JavaScript",
+      title: "Explore Nara",
       channel: {
-        name: "Web Dev Simplified",
-        id: "WebDevSimplified",
-        profileUrl:
-          "https://yt3.ggpht.com/ytc/APkrFKZWeMCsx4Q9e_Hm6nhOOUQ3fv96QGUXiMr1-pPP=s48-c-k-c0x00ffffff-no-rj",
-      },
+        name: "Travel in Japan",
+        id: "TravelInJapan",
+        profileUrl:"https://picsum.photos/200"      },
       views: 257136,
       postedAt: new Date("2023-08-22"),
       duration: 732,
-      thumbnailUrl: "https://i.ytimg.com/vi/d6a8RymS1zI/maxresdefault.jpg",
+      thumbnailUrl: "https://picsum.photos/381/220",
       videoUrl:
         "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
     },
     {
       id: "3",
-      title: "Front-end developer takes on a CSS battle",
+      title: "History of Mankind",
       channel: {
-        name: "Kevin Powell",
-        id: "KevinPowell",
+        name: "Geopop",
+        id: "Geopop",
         profileUrl:
-          "https://yt3.ggpht.com/ytc/APkrFKa6XiLa13mMVPzkmmTBcgNPjjqCGPrY86KfJFmf5w=s48-c-k-c0x00ffffff-no-rj",
-      },
+"https://picsum.photos/202"      },
       views: 1232300,
       postedAt: new Date("2023-10-05"),
       duration: 120,
-      thumbnailUrl: "https://i.ytimg.com/vi/eYPyIq5Y3Rk/maxresdefault.jpg",
+      thumbnailUrl: "https://picsum.photos/379/220",
       videoUrl:
         "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
     },
     {
       id: "4",
-      title: "Quick guide to CSS focus states",
+      title: "Russian Literature",
       channel: {
-        name: "Kevin Powell",
-        id: "KevinPowell",
-        profileUrl:
-          "https://yt3.ggpht.com/ytc/APkrFKa6XiLa13mMVPzkmmTBcgNPjjqCGPrY86KfJFmf5w=s48-c-k-c0x00ffffff-no-rj",
-      },
+        name: "Classics of literature",
+        id: "ClassicsOfLiterature",
+        profileUrl:"https://picsum.photos/203"      },
       views: 112,
       postedAt: new Date("2023-9-19"),
       duration: 4343,
-      thumbnailUrl: "https://i.ytimg.com/vi/apdD69J4bEc/maxresdefault.jpg",
+      thumbnailUrl: "https://picsum.photos/383/220",
       videoUrl:
         "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
     },
     {
       id: "5",
-      title: "I Cannot Believe React Made A Hook For This",
+      title: "Best movies of 2023",
       channel: {
-        name: "Web Dev Simplified",
-        id: "WebDevSimplified",
-        profileUrl:
-          "https://yt3.ggpht.com/ytc/APkrFKZWeMCsx4Q9e_Hm6nhOOUQ3fv96QGUXiMr1-pPP=s48-c-k-c0x00ffffff-no-rj",
-      },
+        name: "Cinematic",
+        id: "Cinematic",
+        profileUrl:"https://picsum.photos/204"      },
       views: 42345,
       postedAt: new Date("2023-03-02"),
       duration: 1000,
-      thumbnailUrl: "https://i.ytimg.com/vi/M3mGY0pgFk0/maxresdefault.jpg",
+      thumbnailUrl: "https://picsum.photos/386/220",
       videoUrl:
         "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
     },
     {
       id: "6",
-      title: "I Got Laid Off...",
+      title: "Best sushi in Tokyo",
       channel: {
-        name: "Caleb Curry",
-        id: "CalebCurry",
-        profileUrl:
-          "https://yt3.googleusercontent.com/ytc/APkrFKbpSojje_-tkBQecNtFuPdSCrg3ZT0FhaYjln9k0g=s176-c-k-c0x00ffffff-no-rj",
-      },
+        name: "Exploring Edo",
+        id: "ExploringEdo",
+        profileUrl:"https://picsum.photos/205"      },
       views: 10340,
       postedAt: new Date("2023-03-03"),
       duration: 54,
-      thumbnailUrl: "https://i.ytimg.com/vi/i2JVQdLnkAY/maxresdefault.jpg",
+      thumbnailUrl: "https://picsum.photos/378/220",
       videoUrl:
         "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
     },
     {
       id: "7",
-      title: "Tails OS in 100 Seconds",
+      title: "Brief history of astronomy",
       channel: {
-        name: "Fireship",
-        id: "Fireship",
-        profileUrl:
-          "https://yt3.googleusercontent.com/ytc/APkrFKb--NH6RwAGHYsD3KfxX-SAgWgIHrjR5E4Jb5SDSQ=s176-c-k-c0x00ffffff-no-rj",
-      },
+        name: "Esa",
+        id: "Esa",
+        profileUrl:"https://picsum.photos/206"      },
       views: 10323340,
       postedAt: new Date("2023-08-09"),
       duration: 100,
-      thumbnailUrl: "https://i.ytimg.com/vi/mVKAyw0xqxw/maxresdefault.jpg",
+      thumbnailUrl: "https://picsum.photos/385/220",
       videoUrl:
         "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
     },
     {
       id: "8",
-      title: "Zig in 100 Seconds",
+      title: "Tokyo's craziest districts ",
       channel: {
-        name: "Fireship",
-        id: "Fireship",
-        profileUrl:
-          "https://yt3.googleusercontent.com/ytc/APkrFKb--NH6RwAGHYsD3KfxX-SAgWgIHrjR5E4Jb5SDSQ=s176-c-k-c0x00ffffff-no-rj",
-      },
+        name: "Exploring Edo",
+        id: "ExploringEdo",
+        profileUrl:"https://picsum.photos/205"      },
       views: 20323340,
       postedAt: new Date("2023-09-09"),
       duration: 105,
-      thumbnailUrl: "https://i.ytimg.com/vi/kxT8-C1vmd4/maxresdefault.jpg",
+      thumbnailUrl: "https://picsum.photos/374/220",
       videoUrl:
         "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
     },
--- src/layouts/PageHeader.tsx
+++ src/layouts/PageHeader.tsx
@@ -2,23 +2,14 @@ import logo from "../assets/logo.png";
 import { Menu, Upload, Bell, User, Mic, Search, ArrowLeft } from "lucide-react";
 import Button from "../components/Button";
 import { useState } from "react";
+import { useSidebarContext } from "../contexts/SidebarContext";
 
 export default function PageHeader() {
   const [showFullWidthSearch, setShowFullWidthSearch] = useState(false);
+
   return (
     <div className="flex gap-10 lg:gap-20 justify-between pt-2 mb-6 mx-4">
-      <div
-        className={` gap-4 items-center flex-shrink-0 ${
-          showFullWidthSearch ? "hidden" : "flex"
-        }`}
-      >
-        <Button variant="ghost" size="icon">
-          <Menu />
-        </Button>
-        <a href="/">
-          <img src={logo} className="h-6" />
-        </a>
-      </div>
+      <PageHeaderFirstSection hidden={showFullWidthSearch} />
       <form
         className={`gap-4 flex-grow justify-center ${
           showFullWidthSearch ? "flex" : "hidden md:flex"
@@ -75,6 +66,31 @@ export default function PageHeader() {
           <User />
         </Button>
       </div>
+    </div>
+  );
+}
+
+type PageHeaderFirstSectionProps = {
+  hidden?: boolean;
+};
+
+export function PageHeaderFirstSection({
+  hidden = false,
+}: PageHeaderFirstSectionProps) {
+  const { toggle } = useSidebarContext();
+
+  return (
+    <div
+      className={` gap-4 items-center flex-shrink-0 ${
+        hidden ? "hidden" : "flex"
+      }`}
+    >
+      <Button onClick={toggle} variant="ghost" size="icon">
+        <Menu />
+      </Button>
+      <a href="/">
+        <img src={logo} className="h-6" />
+      </a>
     </div>
   );
 }
